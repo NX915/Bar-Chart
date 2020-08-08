5e1e609147beb60f8f5586efde90e0a9641773d3
Sean Ni <nixiang915@hotmail.com>
2020-08-08 17:52:42 +0000

    Barchart positioning test

--- assets/bar-chart.js
+++ assets/bar-chart.js
@@ -5,40 +5,51 @@ function drawBarChart(data, option, element) {
       'height': '250px',
       'backgroundColor': 'grey',
       'display': 'block',
+      'border-radius': '10px',
       }
     );
-    $('<div id="barchart_lines"></div>').appendTo(element)
-    $('#barchart_lines').css({
+    $('<div id="barchart"></div>').appendTo(element)
+    $('#barchart').css({
       'width': '0px',
       'height': '0px',
-      'backgroundColor': 'green',
       'position': 'relative',
-      'top': '20px',
+      'top': '220px',
       'left': '20px',
+      'transform': 'rotateX(180deg)',
     });
-    $('<div id="barchart_vertical_line"></div>').appendTo('#barchart_lines');
+    $('<div id="bar1"></div>').appendTo('#barchart')
+    $('#bar1').css({
+      'width': '20px',
+      'height': '100px',
+      'backgroundColor': 'green',
+      'display': 'flex',
+      'left': '20px',
+      'position': 'fixed',
+    });
+    $('<div id="bar2"></div>').appendTo('#barchart')
+    $('#bar2').css({
+      'width': '20px',
+      'height': '150px',
+      'backgroundColor': 'green',
+      'display': 'flex',
+      'left': '60px',
+      'position': 'fixed',
+    });
+    $('<div id="barchart_vertical_line"></div>').appendTo('#barchart');
     $('#barchart_vertical_line').css({
       'width': '2px',
       'height': '200px',
       'backgroundColor': 'black',
+      'position': 'fixed',
     });
-    $('<div id="barchart_horizontal_line"></div>').appendTo('#barchart_lines');
+    $('<div id="barchart_horizontal_line"></div>').appendTo('#barchart');
     $('#barchart_horizontal_line').css({
       'width': '400px',
       'height': '2px',
       'backgroundColor': 'black',
+      'position': 'fixed',
     });
   });
 }
-$(document).ready(function() {
-  // alert('loaded');
-  $('a').addClass('test');
-})
-$('a').click(function(event) {
-  event.preventDefault();
-  $('a').removeClass('test');
-  //alert('clicked a');
-  $(this).hide('slow');
-})
 
 drawBarChart(0,0,'#demo');
