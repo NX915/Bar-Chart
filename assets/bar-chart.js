d8e4acb9472e625dc56590b72bd775ff82874f70
Sean Ni <nixiang915@hotmail.com>
2020-08-09 06:41:44 +0000

    dynamic bar sizes

--- assets/bar-chart.js
+++ assets/bar-chart.js
@@ -1,3 +1,39 @@
+function drawAxis(element) {
+  $('<div id="barchart_vertical_line"></div>').appendTo(element);
+  $('#barchart_vertical_line').css({
+    'width': '3px',
+    'height': '200px',
+    'backgroundColor': 'black',
+    'position': 'fixed',
+  });
+  $('<div id="barchart_horizontal_line"></div>').appendTo(element);
+  $('#barchart_horizontal_line').css({
+    'width': '100%',
+    'height': '3px',
+    'backgroundColor': 'black',
+    'position': 'fixed',
+  });
+}
+
+function drawBars(data, option, element) {
+  const maxH = parseInt($(element).css('height'));
+  let scale = 0;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i] > scale) {
+      scale = data[i];
+    }
+  }
+  scale = maxH / scale;
+  for (let i = 0; i < data.length; i++) {
+    $(('<div id="bar_' + i + '"></div>')).appendTo(element);
+    $(('#bar_' + i)).css({
+      'flex': '0 0 calc(100% / 6)',
+      'height': (data[i] * scale) +'px',
+      'backgroundColor': 'red',
+    });
+  }
+}
+
 function drawBarChart(data, option, element) {
   $(element).css({
     'width': 'auto',
@@ -6,18 +42,18 @@ function drawBarChart(data, option, element) {
     'display': 'block',
     }
   );
-  $('<div id="barchart"></div>').appendTo(element)
+  $('<div id="barchart"></div>').appendTo(element);
   $('#barchart').css({
-    'width': '100%',
+    'width': 'calc(100% - 60px)',
     'height': '100%',
     'position': 'relative',
-    'top': '-20px',
-    'left': '20px',
+    'top': '-30px',
+    'left': '40px',
     'transform': 'rotateX(180deg)',
   });
-  $('<div id="bar_container"></div>').appendTo('#barchart')
+  $('<div id="bar_container"></div>').appendTo('#barchart');
   $('#bar_container').css({
-    'width': '400px',
+    'width': '100%',
     'height': '200px',
     'backgroundColor': 'green',
     'display': 'flex',
@@ -26,38 +62,8 @@ function drawBarChart(data, option, element) {
     'justify-content': 'space-evenly',
     'position': 'fixed',
   });
-  $('<div id="bar1"></div>').appendTo('#bar_container')
-  $('#bar1').css({
-    'width': '20px',
-    'height': '200px',
-    'backgroundColor': 'red',
-  });
-  $('<div id="bar2"></div>').appendTo('#bar_container')
-  $('#bar2').css({
-    'width': '20px',
-    'height': '100px',
-    'backgroundColor': 'blue',
-  });
-  $('<div id="bar3"></div>').appendTo('#bar_container')
-  $('#bar3').css({
-    'width': '20px',
-    'height': '150px',
-    'backgroundColor': 'yellow',
-  });
-  $('<div id="barchart_vertical_line"></div>').appendTo('#barchart');
-  $('#barchart_vertical_line').css({
-    'width': '2px',
-    'height': '200px',
-    'backgroundColor': 'black',
-    'position': 'fixed',
-  });
-  $('<div id="barchart_horizontal_line"></div>').appendTo('#barchart');
-  $('#barchart_horizontal_line').css({
-    'width': '400px',
-    'height': '2px',
-    'backgroundColor': 'black',
-    'position': 'fixed',
-  });
+  drawBars(data,option, '#bar_container');
+  drawAxis('#barchart');
 }
 
-$(document).ready(function () {drawBarChart(0,0,'#demo')});
+$(document).ready(function () {drawBarChart([1, 2, 7, 4, 5],0,'#demo')});
